2ea23f1846de300f6a607c28f7c56924f1f48c01
Nchekwube001 <unekwenchekwube@gmail.com>
2025-03-23 20:24:14 +0100

    Added textfiel

--- SwiftPlayground/ContentView.swift
+++ SwiftPlayground/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
+    @State var isEdit = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter Username",text:$text,onEditingChanged: {
+                isEditing in
+                isEdit = isEditing
+                print(isEditing)
+            })
+            .textFieldStyle(.roundedBorder)
+                .padding()
+                
+            Button("Done"){
+                print(text)
+                UIApplication.shared.onEndEditing()
+            }
         }
         .padding()
+    }
+}
+
+
+extension UIApplication {
+    func onEndEditing(){
+        
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
